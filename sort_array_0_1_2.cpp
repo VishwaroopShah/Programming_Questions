857979c1778e62144e077a0ed094d5d11887fd13
VishwaroopShah <40748423+VishwaroopShah@users.noreply.github.com>
2021-06-06 14:38:10 +0530

    Added Program to sort array of 0's 1's and 2's

---
+++ sort_array_0_1_2.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+void swap(int A[], int pos1, int pos2) {
+    int temp = A[pos1];
+    A[pos1] = A[pos2];
+    A[pos2] = temp;
+}
+
+void sortTheArray(int A[], int arraySize) {
+    //int arrayElements = sizeof(A)/sizeof(A[0]);
+    //A[0] to A[low -1] : contains 0,
+    //A[low] to A[mid -1] : contains 1,
+    //A[mid] to A[high] : contains 2,
+    int low = 0, mid = 0, high = arraySize - 1;
+    while(mid <= high) {
+        switch(A[mid]) {
+        case 0:
+            swap(A, low, mid);
+            low += 1;
+            mid += 1;
+            break;
+        case 1:
+            mid +=1;
+            break;
+        case 2:
+            swap(A, mid, high);
+            high -= 1;
+        }
+    }
+}
+
+void printTheArray(int A[], int arraySize) {
+    //int arraySize = sizeof(A)/sizeof(A[0]);
+    //cout << "array size" << arraySize;
+    for(int i = 0; i < arraySize; i++) {
+        cout << A[i] << " ";
+    }
+}
+
+int main() {
+ int n;
+ cout << "Number of Element :" ;
+ cin >> n;
+ cout<< "Enter Element: " ;
+ int A[n];
+ for(int i = 0; i < n; i++) {
+    cin >> A[i];
+ }
+ sortTheArray(A, n);
+ printTheArray(A, n);
+ return 0;
+}
