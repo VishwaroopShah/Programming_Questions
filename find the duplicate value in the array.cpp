6be6b88d363a24bc7d09129e5c5a8c7ed5a537c9
VishwaroopShah <40748423+VishwaroopShah@users.noreply.github.com>
2021-06-10 22:06:22 +0530

    code to find the duplicate value in the array

---
+++ find the duplicate value in the array.cpp
@@ -0,0 +1,18 @@
+/*
+Problem : https://www.interviewbit.com/problems/find-duplicate-in-array/
+Solution : Used Folyds Cycle Detection (Hare and Tortoise Algorithmn)
+*/
+int Solution::repeatedNumber(const vector<int> &A) {
+    int slow = A[0];
+    int fast = A[A[0]];
+    while(slow!=fast) {
+        slow = A[slow];
+        fast = A[A[fast]];
+    }
+    fast = 0;
+    while(slow != fast) {
+        slow = A[slow];
+        fast = A[fast];
+    }
+    return slow;
+}
